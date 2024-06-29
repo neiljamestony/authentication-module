b3f0865327d5d46dd318c7fb5e8eed0a78d72d71
Perdigon <neil.james.perdigon@accenture.com>
2024-06-29 14:34:22 +0800

    fixed try catch error syntax on registere controller

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -12,43 +12,54 @@ export const registerController = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { email, fname, lname, password, phoneNumber, userType } = req.body;
-  const fieldValidation = handleFieldValidation(req.body);
-  if (!fieldValidation.length) {
-    const isUserExists = await findOneUser({ email });
-    if (isUserExists === null) {
-      const hashedPassword = await generateHash(password);
-      const user = await insertOne({
-        fname: fname,
-        lname: lname,
-        email: email,
-        phoneNumber: phoneNumber,
-        user_type: userType,
-        password: hashedPassword,
-      });
-      if (user) {
-        res.status(201).json({
-          data: [
-            {
-              _id: user._id,
-              fname: user.fname,
-              lname: user.fname,
-              email: user.email,
-              phoneNumber: user.phoneNumber,
-              user_type: user.user_type,
-              token: generateToken(user._id),
-            },
-          ],
-          msg: "success",
+  try {
+    const { email, fname, lname, password, phoneNumber, userType } = req.body;
+    const fieldValidation = handleFieldValidation(req.body);
+    if (!fieldValidation.length) {
+      const isUserExists = await findOneUser({ email });
+      if (isUserExists === null) {
+        const hashedPassword = await generateHash(password);
+        const user = await insertOne({
+          fname: fname,
+          lname: lname,
+          email: email,
+          phoneNumber: phoneNumber,
+          user_type: userType,
+          password: hashedPassword,
         });
+        if (user) {
+          res.status(201).json({
+            data: [
+              {
+                _id: user._id,
+                fname: user.fname,
+                lname: user.fname,
+                email: user.email,
+                phoneNumber: user.phoneNumber,
+                user_type: user.user_type,
+                token: generateToken(user._id),
+              },
+            ],
+            msg: "success",
+          });
+        } else {
+          res.status(201).json({ data: [], msg: "Unexpected Error occured!" });
+        }
       } else {
-        res.status(201).json({ data: [], msg: "Unexpected Error occured!" });
+        res.status(201).json({ data: [], msg: "email exists" });
       }
     } else {
-      res.status(201).json({ data: [], msg: "email exists" });
+      res.status(422).json({ data: [], msg: fieldValidation });
     }
-  } else {
-    res.status(422).json({ data: [], msg: fieldValidation });
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      res.status(500).json({ data: [], msg: error.message });
+      next(error.message);
+    } else {
+      res
+        .status(500)
+        .json({ data: [], msg: `An unexpected error occured: ${error}` });
+    }
   }
 };
 
